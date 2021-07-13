23fc14223561c71eba5542e7843de7531968b26f
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-07-12 21:04:44 -0300

    Se agregaron imagenes en los datos, se mejoraron detalles CSS y se corrigieron detalles de consistencia en javascript

--- app.js
+++ app.js
@@ -40,16 +40,16 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=Rosario&appid=e30242ec8
     
     nombre.innerHTML = nombreValue;
     temp.innerHTML = parseInt(tempValue - 273)+'°C'; //Resta 273 que representan los Kelvin para que quede en grados Celsius
-    humedad.innerHTML = 'Humidity: '+humedadValue+'%';
+    humedad.innerHTML = `Humidity: ${humedadValue}%`;
     presion.innerHTML = `Pressure: ${presionValue/1000}`;
-    desc.innerHTML = descValue;
+    desc.innerHTML = `${descValue}`;
     icon.innerHTML = `<img src="imgs/${iconValue}.png"></img>;`
     viento.innerHTML = `WindStatus ${vientoValue}km/h`;
     pais.innerHTML = `Country: ${paisValue}`;
     visibilidad.innerHTML = `Visibility: ${visibilidadValue/1000}km`;
     sensacionTermica.innerHTML = `Feel like: ${parseInt(termicaValue - 263)}°`;
-    longitud.innerHTML = 'Lon: '+lonValue;
-    latitud.innerHTML = 'Lat: '+latValue;
+    longitud.innerHTML = `Longitud: ${lonValue}`;
+    latitud.innerHTML = `Latitud: ${latValue}`;
 
 //Usando como parametros la longitud y latitud traida de la anterior API consulta a una nueva API para traer datos de dias futuros   
 fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latValue}&lon=${lonValue}&appid=e30242ec84252ed6796422a415839983`)
@@ -65,13 +65,13 @@ fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latValue}&lon=${lon
         var friValue = [new Date(datoFuturo['daily'][6]['dt']*1000).toLocaleString(),datoFuturo['daily'][6]['weather'][0]['icon'],datoFuturo['daily'][6]['temp']['day']];
         var satValue = [new Date(datoFuturo['daily'][7]['dt']*1000).toLocaleString(),datoFuturo['daily'][7]['weather'][0]['icon'],datoFuturo['daily'][7]['temp']['day']];
         hoy.innerHTML = `<p>${hoyValue.substring(0,9)}</p>`;
-        sun.innerHTML = `<p>${sunValue[0].substring(0,4)}</p><img src="imgs/${sunValue[1]}.png"></img><p>${parseInt(sunValue[2] - 273)}°</p>`;
-        mon.innerHTML = `<p>${monValue[0].substring(0,4)}</p><img src="imgs/${monValue[1]}.png"></img><p>${parseInt(monValue[2] - 273)}°</p>`;
-        tue.innerHTML = `<p>${tueValue[0].substring(0,4)}</p><img src="imgs/${tueValue[1]}.png"></img><p>${parseInt(tueValue[2] - 273)}°</p>`;
-        wed.innerHTML = `<p>${wedValue[0].substring(0,4)}</p><img src="imgs/${wedValue[1]}.png"></img><p>${parseInt(wedValue[2] - 273)}°</p>`;
-        thu.innerHTML = `<p>${thuValue[0].substring(0,4)}</p><img src="imgs/${thuValue[1]}.png"></img><p>${parseInt(thuValue[2] - 273)}°</p>`;
-        fri.innerHTML = `<p>${friValue[0].substring(0,4)}</p><img src="imgs/${friValue[1]}.png"></img><p>${parseInt(friValue[2] - 273)}°</p>`;
-        sat.innerHTML = `<p>${satValue[0].substring(0,4)}</p><img src="imgs/${satValue[1]}.png"></img><p>${parseInt(satValue[2] - 273)}°</p>`;
+        sun.innerHTML = `<p>${sunValue[0].substring(0,4)}</p><img src="imgs/${sunValue[1]}.png"></img><p>${parseInt(sunValue[2] - 273)}°C</p>`;
+        mon.innerHTML = `<p>${monValue[0].substring(0,4)}</p><img src="imgs/${monValue[1]}.png"></img><p>${parseInt(monValue[2] - 273)}°C</p>`;
+        tue.innerHTML = `<p>${tueValue[0].substring(0,4)}</p><img src="imgs/${tueValue[1]}.png"></img><p>${parseInt(tueValue[2] - 273)}°C</p>`;
+        wed.innerHTML = `<p>${wedValue[0].substring(0,4)}</p><img src="imgs/${wedValue[1]}.png"></img><p>${parseInt(wedValue[2] - 273)}°C</p>`;
+        thu.innerHTML = `<p>${thuValue[0].substring(0,4)}</p><img src="imgs/${thuValue[1]}.png"></img><p>${parseInt(thuValue[2] - 273)}°C</p>`;
+        fri.innerHTML = `<p>${friValue[0].substring(0,4)}</p><img src="imgs/${friValue[1]}.png"></img><p>${parseInt(friValue[2] - 273)}°C</p>`;
+        sat.innerHTML = `<p>${satValue[0].substring(0,4)}</p><img src="imgs/${satValue[1]}.png"></img><p>${parseInt(satValue[2] - 273)}°C</p>`;
 
     })       
 })
@@ -97,18 +97,18 @@ boton.addEventListener('click', function(){
         
         nombre.innerHTML = nombreValue;
         temp.innerHTML = parseInt(tempValue - 273)+'°C'; //Resta 273 que representan los Kelvin para que quede en grados Celsius
-        humedad.innerHTML = 'Humidity: '+humedadValue+'%';
-        presion.innerHTML = 'Pressure: '+presionValue/1000;
-        desc.innerHTML = descValue;
-        icon.innerHTML = '<img src="imgs/'+iconValue+'.png"></img>'
-        viento.innerHTML = 'WindStatus '+vientoValue +' km/h';
-        pais.innerHTML = 'Country: '+paisValue;
-        visibilidad.innerHTML = 'Visibility: '+visibilidadValue/1000+'km';
-        sensacionTermica.innerHTML = 'Feel like: '+parseInt(termicaValue - 273)+'°';
-        longitud.innerHTML = 'Lon: '+lonValue;
-        latitud.innerHTML = 'Lat: '+latValue;
+        humedad.innerHTML = `Humidity: ${humedadValue}%`;
+        presion.innerHTML = `Pressure: ${presionValue/1000}`;
+        desc.innerHTML = `${descValue}`;
+        icon.innerHTML = `<img src="imgs/${iconValue}.png"></img>;`
+        viento.innerHTML = `WindStatus ${vientoValue}km/h`;
+        pais.innerHTML = `Country: ${paisValue}`;
+        visibilidad.innerHTML = `Visibility: ${visibilidadValue/1000}km`;
+        sensacionTermica.innerHTML = `Feel like: ${parseInt(termicaValue - 263)}°`;
+        longitud.innerHTML = `Longitud: ${lonValue}`;
+        latitud.innerHTML = `Latitud: ${latValue}`;
 
-        //Usando como parametros la longitud y latitud traida de la anterior API consulta a una nueva API para traer datos de dias futuros
+    //Usando como parametros la longitud y latitud traida de la anterior API consulta a una nueva API para traer datos de dias futuros
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latValue}&lon=${lonValue}&appid=e30242ec84252ed6796422a415839983`)
         .then(response => response.json())
         .then(datoFuturo => {
@@ -121,21 +121,17 @@ boton.addEventListener('click', function(){
             var friValue = [new Date(datoFuturo['daily'][6]['dt']*1000).toLocaleString(),datoFuturo['daily'][6]['weather'][0]['icon'],datoFuturo['daily'][6]['temp']['day']];
             var satValue = [new Date(datoFuturo['daily'][7]['dt']*1000).toLocaleString(),datoFuturo['daily'][7]['weather'][0]['icon'],datoFuturo['daily'][7]['temp']['day']];
             hoy.innerHTML = `<p>${hoyValue.substring(0,9)}</p>`;
-            sun.innerHTML = `<p>${sunValue[0].substring(0,4)}</p><img src="imgs/${sunValue[1]}.png"></img><p>${parseInt(sunValue[2] - 273)}°</p>`;
-            mon.innerHTML = `<p>${monValue[0].substring(0,4)}</p><img src="imgs/${monValue[1]}.png"></img><p>${parseInt(monValue[2] - 273)}°</p>`;
-            tue.innerHTML = `<p>${tueValue[0].substring(0,4)}</p><img src="imgs/${tueValue[1]}.png"></img><p>${parseInt(tueValue[2] - 273)}°</p>`;
-            wed.innerHTML = `<p>${wedValue[0].substring(0,4)}</p><img src="imgs/${wedValue[1]}.png"></img><p>${parseInt(wedValue[2] - 273)}°</p>`;
-            thu.innerHTML = `<p>${thuValue[0].substring(0,4)}</p><img src="imgs/${thuValue[1]}.png"></img><p>${parseInt(thuValue[2] - 273)}°</p>`;
-            fri.innerHTML = `<p>${friValue[0].substring(0,4)}</p><img src="imgs/${friValue[1]}.png"></img><p>${parseInt(friValue[2] - 273)}°</p>`;
-            sat.innerHTML = `<p>${satValue[0].substring(0,4)}</p><img src="imgs/${satValue[1]}.png"></img><p>${parseInt(satValue[2] - 273)}°</p>`;
+            sun.innerHTML = `<p>${sunValue[0].substring(0,4)}</p><img src="imgs/${sunValue[1]}.png"></img><p>${parseInt(sunValue[2] - 273)}°C</p>`;
+            mon.innerHTML = `<p>${monValue[0].substring(0,4)}</p><img src="imgs/${monValue[1]}.png"></img><p>${parseInt(monValue[2] - 273)}°C</p>`;
+            tue.innerHTML = `<p>${tueValue[0].substring(0,4)}</p><img src="imgs/${tueValue[1]}.png"></img><p>${parseInt(tueValue[2] - 273)}°C</p>`;
+            wed.innerHTML = `<p>${wedValue[0].substring(0,4)}</p><img src="imgs/${wedValue[1]}.png"></img><p>${parseInt(wedValue[2] - 273)}°C</p>`;
+            thu.innerHTML = `<p>${thuValue[0].substring(0,4)}</p><img src="imgs/${thuValue[1]}.png"></img><p>${parseInt(thuValue[2] - 273)}°C</p>`;
+            fri.innerHTML = `<p>${friValue[0].substring(0,4)}</p><img src="imgs/${friValue[1]}.png"></img><p>${parseInt(friValue[2] - 273)}°C</p>`;
+            sat.innerHTML = `<p>${satValue[0].substring(0,4)}</p><img src="imgs/${satValue[1]}.png"></img><p>${parseInt(satValue[2] - 273)}°C</p>`;
         })      
-    })
-    
+    })   
     .catch(err => alert("La ciudad no se encuentra"))
     })
-
-
-
 
 
 const unixTimestamp = 1626121380
