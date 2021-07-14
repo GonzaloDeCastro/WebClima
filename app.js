3d926b458d11d8410ea07391fa2067d5ceace8d8
Gonzalo De Castro <gonzalodecastro1@gmail.com>
2021-07-14 19:40:57 -0300

    Se optimiza parte del codigo javascript aplicando objetos, buclea for y se crean elementos html

--- app.js
+++ app.js
@@ -12,14 +12,46 @@ var sensacionTermica = document.querySelector('.sensacionTermica');
 var presion = document.querySelector('.presion');
 var latitud = document.querySelector('.latitud');
 var longitud = document.querySelector('.longitud');
-var sem1 = document.querySelector('.sem1');
-var sem2 = document.querySelector('.sem2');
-var sem3 = document.querySelector('.sem3');
-var sem4 = document.querySelector('.sem4');
-var sem5 = document.querySelector('.sem5');
-var sem6 = document.querySelector('.sem6');
-var sem7 = document.querySelector('.sem7');
-var sem0 = document.querySelector('.sem0');
+var contenedorDias = document.querySelector(".contenedorDias");
+var sem0 = document.getElementById('sem0'); 
+
+for (var i = 1; i < 8; i++) {
+        var sem = document.createElement('div');
+        var par = document.createElement('p');
+        var parimg = document.createElement('img');
+        var par2 = document.createElement('p');
+        sem.id = 'sem'+ i;
+        sem.appendChild(par)
+        sem.appendChild(parimg)
+        sem.appendChild(par2)
+        contenedorDias.appendChild(sem);
+    }
+
+var semanas = [
+    {
+        sem1:document.getElementById('sem1')
+    },
+    {
+        sem2:document.getElementById('sem2')
+    },
+    {
+        sem3:document.getElementById('sem3')
+    },
+    {
+        sem4:document.getElementById('sem4')
+    },
+    {
+        sem5:document.getElementById('sem5')
+    },
+    {
+        sem6:document.getElementById('sem6')
+    },
+    {
+        sem7:document.getElementById('sem7')
+    },
+
+];
+
 
 //Valores por defecto traidor por la API seleccionando la ciudad de Rosario
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=Rosario&appid=e30242ec84252ed6796422a415839983`)
@@ -57,26 +89,18 @@ fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latValue}&lon=${lon
     .then(datoFuturo => {
         console.log(datoFuturo)
         var dia0 = new Date(datoFuturo.daily[0].dt*1000).toLocaleString();
-        var dia1 = [new Date(datoFuturo.daily[1].dt*1000).toLocaleString(),datoFuturo.daily[1].weather[0].icon,datoFuturo.daily[1].temp.day];
-        var dia2 = [new Date(datoFuturo.daily[2].dt*1000).toLocaleString(),datoFuturo.daily[2].weather[0].icon,datoFuturo.daily[2].temp.day];
-        var dia3 = [new Date(datoFuturo.daily[3].dt*1000).toLocaleString(),datoFuturo.daily[3].weather[0].icon,datoFuturo.daily[3].temp.day];
-        var dia4 = [new Date(datoFuturo.daily[4].dt*1000).toLocaleString(),datoFuturo.daily[4].weather[0].icon,datoFuturo.daily[4].temp.day];
-        var dia5 = [new Date(datoFuturo.daily[5].dt*1000).toLocaleString(),datoFuturo.daily[5].weather[0].icon,datoFuturo.daily[5].temp.day];
-        var dia6 = [new Date(datoFuturo.daily[6].dt*1000).toLocaleString(),datoFuturo.daily[6].weather[0].icon,datoFuturo.daily[6].temp.day];
-        var dia7 = [new Date(datoFuturo.daily[7].dt*1000).toLocaleString(),datoFuturo.daily[7].weather[0].icon,datoFuturo.daily[7].temp.day];
-
-        for (i = 0; i<8; i++){
-                console.log('prueba'+i);
+        for (i = 1; i<= 7; i++){
+            semanas[i] = [new Date(datoFuturo.daily[i].dt*1000).toLocaleString(),datoFuturo.daily[i].weather[0].icon,datoFuturo.daily[i].temp.day];
         }
 
         sem0.innerHTML = `<p>${dia0.substring(0,9)}</p>`;
-        sem1.innerHTML = `<p>${dia1[0].substring(0,4)}</p><img src="imgs/${dia1[1]}.png"></img><p>${parseInt(dia1[2] - 273)}°C</p>`;
-        sem2.innerHTML = `<p>${dia2[0].substring(0,4)}</p><img src="imgs/${dia2[1]}.png"></img><p>${parseInt(dia2[2] - 273)}°C</p>`;
-        sem3.innerHTML = `<p>${dia3[0].substring(0,4)}</p><img src="imgs/${dia3[1]}.png"></img><p>${parseInt(dia3[2] - 273)}°C</p>`;
-        sem4.innerHTML = `<p>${dia4[0].substring(0,4)}</p><img src="imgs/${dia4[1]}.png"></img><p>${parseInt(dia4[2] - 273)}°C</p>`;
-        sem5.innerHTML = `<p>${dia5[0].substring(0,4)}</p><img src="imgs/${dia5[1]}.png"></img><p>${parseInt(dia5[2] - 273)}°C</p>`;
-        sem6.innerHTML = `<p>${dia6[0].substring(0,4)}</p><img src="imgs/${dia6[1]}.png"></img><p>${parseInt(dia6[2] - 273)}°C</p>`;
-        sem7.innerHTML = `<p>${dia7[0].substring(0,4)}</p><img src="imgs/${dia7[1]}.png"></img><p>${parseInt(dia7[2] - 273)}°C</p>`;
+        sem1.innerHTML = `<p>${semanas[1][0].substring(0,4)}</p><img src="imgs/${semanas[1][1]}.png"></img><p>${parseInt(semanas[1][2] - 273)}°C</p>`;
+        sem2.innerHTML = `<p>${semanas[2][0].substring(0,4)}</p><img src="imgs/${semanas[2][1]}.png"></img><p>${parseInt(semanas[2][2] - 273)}°C</p>`;
+        sem3.innerHTML = `<p>${semanas[3][0].substring(0,4)}</p><img src="imgs/${semanas[3][1]}.png"></img><p>${parseInt(semanas[3][2] - 273)}°C</p>`;
+        sem4.innerHTML = `<p>${semanas[4][0].substring(0,4)}</p><img src="imgs/${semanas[4][1]}.png"></img><p>${parseInt(semanas[4][2] - 273)}°C</p>`;
+        sem5.innerHTML = `<p>${semanas[5][0].substring(0,4)}</p><img src="imgs/${semanas[5][1]}.png"></img><p>${parseInt(semanas[5][2] - 273)}°C</p>`;
+        sem6.innerHTML = `<p>${semanas[6][0].substring(0,4)}</p><img src="imgs/${semanas[6][1]}.png"></img><p>${parseInt(semanas[6][2] - 273)}°C</p>`;
+        sem7.innerHTML = `<p>${semanas[7][0].substring(0,4)}</p><img src="imgs/${semanas[7][1]}.png"></img><p>${parseInt(semanas[7][2] - 273)}°C</p>`;
 
     })       
 })
@@ -133,8 +157,6 @@ boton.addEventListener('click', function(){
             sem5.innerHTML = `<p>${dia5[0].substring(0,4)}</p><img src="imgs/${dia5[1]}.png"></img><p>${parseInt(dia5[2] - 273)}°C</p>`;
             sem6.innerHTML = `<p>${dia6[0].substring(0,4)}</p><img src="imgs/${dia6[1]}.png"></img><p>${parseInt(dia6[2] - 273)}°C</p>`;
             sem7.innerHTML = `<p>${dia7[0].substring(0,4)}</p><img src="imgs/${dia7[1]}.png"></img><p>${parseInt(dia7[2] - 273)}°C</p>`;
-
-          
         })      
     })   
     .catch(err => alert("La ciudad no se encuentra"))
@@ -153,3 +175,4 @@ console.log(humanDateFormat)
 
 
 const abrevio = new Date(1626120000 * 1000).toLocaleString()
+
